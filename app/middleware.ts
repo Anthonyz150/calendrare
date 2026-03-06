e710aa4823ef67e27d82689ee043b5c89037f412
Anthonyz150 <anthony.didier.pro@gmail.com>
2026-03-06 16:29:40 +0100

    Calendrare

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,7 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({});
- 
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// On définit quelles routes sont publiques (tout le monde peut voir)
+// Ici, on veut que TOUT soit protégé sauf la page de connexion
+const isProtectedRoute = createRouteMatcher(['/']); 
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
