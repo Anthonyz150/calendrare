707cd1c8d8543f37e18fb9392fb331a5b3a0b017
Anthonyz150 <anthony.didier.pro@gmail.com>
2026-03-06 18:07:33 +0100

    Calendrare

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider publishableKey="pk_test_bmV3LXNocmV3LTIyLmNsZXJrLmFjY291bnRzLmRldiQ">
-      <html lang="fr">
+<ClerkProvider publishableKey="pk_test_bmV3LXNocmV3LTIyLmNsZXJrLmFjY291bnRzLmRldiQ">
+  <html lang="fr">
         {/* On force le bg-black et overflow-hidden pour un rendu propre */}
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black overflow-hidden m-0 p-0`}>
           {/* On a enlevé le <header> moche car on a déjà notre propre UserButton dans page.tsx */}
