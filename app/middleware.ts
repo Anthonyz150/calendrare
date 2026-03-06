6b828f91882e5dbc9004f979c13afd6904b80480
Anthonyz150 <anthony.didier.pro@gmail.com>
2026-03-06 17:30:39 +0100

    Calendrare

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,20 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// On définit quelles routes doivent être protégées
-const isProtectedRoute = createRouteMatcher(['/']); 
-
-export default clerkMiddleware(async (auth, req) => {
-  // On appelle protect() directement sur le résultat de auth()
-  if (isProtectedRoute(req)) {
-    await auth.protect(); 
-  }
-});
+// On ne définit aucune route protégée pour l'instant
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Ignore les fichiers statiques et les trucs internes de Next.js
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Toujours exécuter pour les routes API
     '/(api|trpc)(.*)',
   ],
 };
